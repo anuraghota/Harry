f3099fd396f20ede808e8ca6d8fc073cc816cf9b
Anurag Hota <ahota92@gmail.com>
2016-02-26 00:57:50 +0530

    Update cli.py

--- prototype/app/cli.py
+++ prototype/app/cli.py
@@ -1,5 +1,10 @@
 import sys
 import prototype.data.common_responses as response
+import prototype.data.machine_questions as que
+import prototype.data.user_data as user
+from time import time
+import msvcrt
+
 def start():
     chat()
 
@@ -10,7 +15,31 @@ def chat():
     while True:
         user_message = str(input())
         last_harry_message = response.get_response(user_message)
+        if last_harry_message == "name":
+            last_harry_message = user.get_name()
         print(last_harry_message)
+        if user_message.lower() == 'bye':
+            break
+
+        if que.question_tags.get("name"):
+            pass
+        else:
+            last_harry_message = que.questions.get("name")
+            print(last_harry_message)
+            user_message = str(input())
+            que.set_tag("name")
+            user.set_name(user_message)
+            last_harry_message = user_message + "! Such a nice name!"
+            print(last_harry_message)
+
+
+
+
+
+
+
+
+
 
 
 
