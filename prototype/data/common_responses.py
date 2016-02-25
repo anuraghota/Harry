e7d432ec7b1eac86e3e67dd6181fdbcc35eb361b
Anurag Hota <ahota92@gmail.com>
2016-02-25 20:53:45 +0530

    Added common responses

--- prototype/data/common_responses.py
+++ prototype/data/common_responses.py
@@ -1,1 +1,40 @@
 __author__='Anurag'
+
+from user_data import user_information
+
+def get_name():
+    name = user_information.get("name")
+    if name is None:
+        name = "You haven't told me yet!"
+    return name
+
+responses = \
+    {
+        "hi": "Hello!Ssup",
+        "what is your name": "Harry! Not Potter though. I am just Harry.",
+        "who is your father": "Anurag! I love my daddy.",
+        "who's your father": "Anurag! I love my daddy.",
+        "who is your mother": "My dad is single.",
+        "who's your mother": "My dad is single.",
+        "say my name": get_name(),
+        "what is my name": get_name(),
+        "what's my name": get_name()
+    }
+
+
+def get_response(query):
+    query = query.lower()
+    response = responses.get(query)
+    if response is None:
+        response = "Didn't get you!My vocabulary is improving day by day."
+    return response
+
+
+
+
+if __name__ == '__main__':
+    query = 'Say my name'
+    result = get_response(query)
+    print(result)
+
+
