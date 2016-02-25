812b49b958234e8ee1ac0c4d565557bf063acc83
Anurag Hota <ahota92@gmail.com>
2016-02-25 22:34:49 +0530

    Added general questions for machine

---
+++ prototype/data/machine_questions.py
@@ -0,0 +1,12 @@
+__author__='Anurag'
+
+questions = \
+    {
+        "name": "What is your name?",
+        "age": "What's your age?",
+        "gender": "Are you male or female?",
+        "marriage_status": "Are you single?",
+        "work_status": "Where do you work?",
+        "home_adrress": "Your home address?",
+        "office_adrress": "Your office address?"
+    }
